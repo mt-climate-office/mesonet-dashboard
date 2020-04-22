0a56281fbada182500eaa5a7fae3e8b28f4d0b22
Zach Hoylman <zhoylman@gmail.com>
2020-04-22 12:05:00 -0600

    updated dashboard tables and units

--- R/mesonet-update.R
+++ R/mesonet-update.R
@@ -12,10 +12,6 @@ library(kableExtra)##
 library(units)##
 library(lubridate) ##
 
-# httr::GET(url = "https://cfcmesonet.cfc.umt.edu/api/latest", 
-#           query = list(stations=c("arskeogh"),type = "csv")) %>%
-#   httr::content()
-
 # var set root dir
 setwd("/home/zhoylman/")
 
@@ -50,9 +46,9 @@ latest = getURL("https://cfcmesonet.cfc.umt.edu/api/latest?tz=US%2FMountain&wide
                    if(units == "mm") value_unit %>% set_units("in") else
                      if(units == "%") value_unit %>% set_units("%") else
                        if(units == "W/m²") value_unit %>% set_units("W/m²") else
-                         if(units == "kPa") value_unit %>% set_units("kPa") else
+                         if(units == "kPa") value_unit %>% set_units("bar") else
                            if(units == "°") value_unit %>% set_units("°") else
-                             if(units == "m/s") value_unit %>% set_units("ft/s") else
+                             if(units == "m/s") value_unit %>% set_units("mi/hr") else
                                if(units == "mV") value_unit %>% set_units("mV") else NA) %>%
   mutate(new_units = 
            if(units == "°C") "°F"  else
@@ -62,12 +58,15 @@ latest = getURL("https://cfcmesonet.cfc.umt.edu/api/latest?tz=US%2FMountain&wide
                    if(units == "mm") "in" else
                      if(units == "%") "%" else
                        if(units == "W/m²") "W/m²" else
-                         if(units == "kPa") "kPa" else
+                         if(units == "kPa") "bars" else
                            if(units == "°") "°" else
-                             if(units == "m/s") "ft/s" else
+                             if(units == "m/s") "mph" else
                                if(units == "mV") "mV" else NA) %>%
   mutate(new_value = round(new_value, 2)) %>%
   mutate(value_unit_new = mixed_units(new_value, new_units))
+
+#rename precip
+latest$long_name = stringr::str_replace(latest$long_name, "Net precipitation since previous report", 'Precipitation since previous report')
 
 #manual
 simple_plotly = function(data,name_str,col,ylab,conversion_func){
@@ -85,12 +84,12 @@ simple_plotly = function(data,name_str,col,ylab,conversion_func){
 #define inputs (add precip when ready)
 names_str = c("sol_radi", "air_temp", "rel_humi", "wind_spd")
 col = c('red', 'green', 'black', "orange")
-ylab = c("Solar Radiation\n(W/m<sup>2</sup>)", "Air Temperature\n(°F)", "Relative Humidity\n(%)", "Wind Speed\n(ft/s)")
-target_unit = c("W/m²", "°F", "%", "ft/s")
+ylab = c("Solar Radiation\n(W/m<sup>2</sup>)", "Air Temperature\n(°F)", "Relative Humidity\n(%)", "Wind Speed\n(mph)")
+target_unit = c("W/m²", "°F", "%", "mph")
 conversion_func = list(function(x){return(x)}, 
                        function(x){return((x * 9/5)+32)},
                        function(x){return(x)}, 
-                       function(x){return(x * 3.28084)})
+                       function(x){return(x * 2.237)})
 
 #loop though stations
 cl = makeCluster(4)
@@ -187,10 +186,20 @@ foreach(s=1:length(stations$`Station ID`)) %dopar% {
     select(datetime)%>%
     head(1)
   
+  vars_of_interest = c('Air Temperature', 'Relative humidity', 'Wind direction', 'Wind speed',
+                       'Maximum wind gust speed since previous report', 'Precipitation since previous report',
+                       'Maximum precipiation rate', 'Atmospheric Pressure', 'Solar radiation',
+                       'Soil volumetric water content at 0\"','Soil volumetric water content at 8\"',
+                       'Soil volumetric water content at 20\"', 'Soil volumetric water content at 36\"',
+                       'Soil temperature at 0\"', 'Soil temperature at 8\"', 'Soil temperature at 20\"',
+                       'Soil temperature at 36\"', 'Vapor pressure deficit', 'Battery Percent', 'Battery Voltage')
+  
   latest %>%
     filter(station_key == stations$`Station ID`[s]) %>% 
     select("long_name", "value_unit_new", "new_units") %>%
     rename(Observation = long_name, Value = value_unit_new, Units = new_units)%>%
+    dplyr::filter(Observation %in% vars_of_interest) %>%
+    arrange(factor(Observation, levels = vars_of_interest))%>%
     kable(., "html", caption = paste0("Latest observation was at ", latest_time[1]$datetime %>% as.character()))%>%
     kable_styling(bootstrap_options = c("striped", "hover", "condensed", "responsive"))%>%
     save_kable(file = paste0("/home/zhoylman/mesonet-dashboard/data/station_page/latest_table/",stations$`Station ID`[s],"_current_table.html"),  self_contained = F, libdir = "./libs")
