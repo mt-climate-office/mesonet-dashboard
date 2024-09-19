a46b476d72de6fb8ae1939429244bac0c328aef8
colinbrust <colin.brust@gmail.com>
2024-09-19 09:34:58 -0600

    Make monthly aggregations work for derived variables in downloader.

--- app/mdb/utils/params.py
+++ app/mdb/utils/params.py
@@ -87,10 +87,13 @@ class params:
         "Gust Speed @ 8 ft [mi/hr]",
         "Gust Speed @ 10 m [mi/hr]",
         "Well Water Level [in]",
+        'Reference ET (a=0.23) [in]',
+        'Comprehensive Climate Index [°F]',
+        'Feels Like Temperature [°F]',
     ]
 
     agg_funcs = {
-        item: "sum" if item == "Precipitation [in]" else "mean" for item in elem_labs
+        item: "sum" if item in ["Precipitation [in]", 'Reference ET (a=0.23) [in]'] else "mean" for item in elem_labs
     }
 
     lab_swap = {
